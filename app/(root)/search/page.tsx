aa82c3601ba366018360f5668311c9d72c34ffbe
shiva kumar <98257507+Shivuduu19@users.noreply.github.com.com>
2024-05-30 17:32:36 +0530

    add searchbar and functionlity

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -8,8 +8,13 @@ import Image from "next/image"
 import { redirect } from "next/navigation"
 import ThreadsTab from "@/components/shared/ThreadsTab"
 import UserCard from "@/components/cards/UserCard"
+import Searchbar from "@/components/shared/SearchBar"
 
-const Page = async () => {
+const Page = async ({
+    searchParams,
+}: {
+    searchParams: { [key: string]: string | undefined };
+}) => {
     const user = await currentUser()
     // console.log(user);
 
@@ -18,11 +23,13 @@ const Page = async () => {
     const userInfo = await fetchUser(user.id)
 
     if (!userInfo?.onboarded) redirect('/onboarding')
+    console.log(searchParams);
+
     // fetch users 
     const result = await fetchUsers({
         userId: user.id,
-        searchString: '',
-        pageNumber: 1,
+        searchString: searchParams.q,
+        pageNumber: searchParams?.page ? +searchParams.page : 1,
         pageSize: 25,
     })
 
@@ -31,6 +38,7 @@ const Page = async () => {
             <h1 className="head-text mb-10"> Search</h1>
 
             {/* search bar */}
+            <Searchbar routeType='search' />
             <div className="mt-14 flex flex-col gap-9">
                 {
                     result.users.length === 0 ? (
